6717aa7c9493736770534f5e8f84d651c857a4da
Guilherme Veloso <velosologan@gmail.com>
2020-04-06 22:18:52 -0300

    Headers e Life cycle

---
+++ src/br/upis/Servlet7.java
@@ -0,0 +1,19 @@
+package br.upis;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/setimo")
+public class Servlet7 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("\n");
+		response.getWriter().append("User-Agent: " + request.getHeader("User-Agent"));
+	}
+}
---
+++ src/br/upis/Servlet8.java
@@ -0,0 +1,33 @@
+package br.upis;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet8
+ */
+@WebServlet("/oitavo")
+public class Servlet8 extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+    
+	private int contador = 0;
+    
+	@Override
+	public void init() throws ServletException {
+		System.out.println("(INIT)Contador: " + contador);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Contador: ").append(""+ ++contador);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("(DESTROY) Contador: " + contador);
+	}	
+}
